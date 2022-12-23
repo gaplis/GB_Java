9061fc94ed33d136ebbd5b9bb03e84244582db56
IlyaChistov <gaplis322@gmail.com>
2022-12-23 11:43:58 +0700

    Lection 3

---
+++ Lections/Lection_3/_001.java
@@ -0,0 +1,14 @@
+package Lections.Lection_3;
+
+import java.util.ArrayList;
+
+public class _001 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(2809);
+
+        for (Object o : list) {
+                System.out.println(o);
+        }
+    }
+}
---
+++ Lections/Lection_3/_003.java
@@ -0,0 +1,20 @@
+package Lections.Lection_3;
+
+import java.util.*;
+
+public class _003 {
+    public static void main(String[] args) {
+        List<Integer> list = List.of(1, 12, 123, 1234, 12345);
+
+        for (int item : list) {
+            System.out.println(item);
+        }
+
+        Iterator<Integer> col = list.iterator();
+        System.out.println();
+
+        while (col.hasNext()) {
+            System.out.println(col.next());
+        }
+    }
+}
